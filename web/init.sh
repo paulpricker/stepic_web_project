0f1167c62453fcd7bd2b9fb1c045bfd9ba254450
paulpricker <paulpricker@gmail.com>
2016-11-15 02:07:56 +0300

    0.003

--- web/init.sh
+++ web/init.sh
@@ -1,2 +1,2 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/stepic_web_project/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
